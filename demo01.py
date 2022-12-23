3a69cf8817889b65f25f5fa0c5a6436deae50237
v_xboxiao <79147363@qq.com>
2022-12-23 17:50:01 +0800

    adds_rule批量添加

--- demo01.py
+++ demo01.py
@@ -7,7 +7,7 @@ def logging(funcDesc=None):  # 用于装饰的函数名
     def outwrapper(func):  # 接收 用于调用的函数名
         def wrapper(*args, **kwargs):  # 接收 传递到调用函数的参数
             logger.info(f"{funcDesc},调用:{func.__name__}()")
-            logger.info(f"请求参数 {func.__name__}(): {kwargs}")
+            logger.info(f"请求参数 {func.__name__}(): {args,kwargs}")
             returnData = func(*args, **kwargs)  # 调用被装饰的函数
             logger.debug(f"{'返回数据'} {func.__name__}():{returnData}")
             return returnData
@@ -20,18 +20,36 @@ def logging(funcDesc=None):  # 用于装饰的函数名
 class TestPlatform:
 
     def __init__(self):
-        conn = pymysql.connect(host=HOST,port=PORT,user=USER,password=PASSWORD,database='test_platform',charset='utf8')
-        self.cursor = conn.cursor()
+        self.db = pymysql.connect(host=HOST,port=PORT,user=USER,password=PASSWORD,database='test_platform',charset='utf8')
+        self.cursor = self.db.cursor()
+
+    @logger.catch
+    @logging("获取所有字段名及默认值")
+    def getFieldDefaultValue(self, tableName):
+        """
+        获取所有字段名及默认值
+        :param tableName: 表名
+        :return:
+        """
+        # 查询 默认
+        sql = f"select COLUMN_NAME,COLUMN_TYPE  from information_schema.COLUMNS where table_name = '{tableName}';"
+        self.cursor.execute(sql)
+        fieldsdefaultValue = {}     # 存储 默认值
+        for line in self.cursor.fetchall():
+            if line[0] == 'id':
+                continue
+            fieldsdefaultValue[line[0]] = None if 'varchar' in line[1] else 0
+        return fieldsdefaultValue
 
     @logger.catch
     @logging("查询数据")
     def query(self, tableName, tableField, keyWord=None):
         """
-        查询语句 条件关系and
-        :param tableName: 表名 ‘rule’
+        查询数据 条件关系and
+        :param tableName: 表名
         :param keyWord: 查询条件 {'id': 1, 'ruleName': '微众身份证影印件合规规则'}
         :param tableField: 获取字段数据['id','ruleName']
-        :return: data 列表嵌套字典 [{'id': 1, 'ruleName': '微众身份证影印件合规规则'}]
+        :return: dataList 列表嵌套字典 [{'id': 1, 'ruleName': '微众身份证影印件合规规则'}]
         """
         if keyWord:
             keyWordList = [f'{key}="{keyWord[key]}"' if isinstance(keyWord[key], str) else f'{key}={keyWord[key]}' for key in keyWord.keys()]
@@ -42,14 +60,53 @@ class TestPlatform:
         logger.debug(f'SQL：{sql}')
         self.cursor.execute(sql)
 
-        data = []
+        dataList = []
         for line in self.cursor.fetchall():
             lineData = {}
             for field in range(len(line)):
                 lineData[tableField[field]] = line[field]
-            data.append(lineData)
-        return data
+            dataList.append(lineData)
+        return dataList
 
+    @logger.catch
+    @logging("单挑添加数据")
+    def addOnce(self, tableName, data):
+        """
+        写入数据
+        :param tableName: 表名
+        :param data: data 字典 {'id': 1, 'ruleName': '微众身份证影印件合规规则'}
+        :return:
+        """
+        keysList = [key for key in data.keys()]
+        valueList = [f'"{value}"' if isinstance(value, str) else value for value in data.values()]
+        sql = f"insert into {tableName}({','.join(keysList)}) values({','.join(valueList)});"
+        logger.debug(f'SQL：{sql}')
+        self.cursor.execute(sql)
+        self.db.commit()
+        for line in self.cursor.fetchall():
+            print('line:', line)
+
+    @logger.catch
+    @logging("批量添加数据")
+    def addBatch(self, tableName, dataList):
+        """
+
+        :param tableName: 表名
+        :param dataList:列表嵌套字典 [{'id': 1, 'ruleName': '微众身份证影印件合规规则'}]
+        :return:
+        """
+        # 查询 默认值，补全空字段
+
+        defaultValueList = []
+        for data in dataList:
+            defaultValue = [data[key] for key in data.keys()]
+            defaultValueList.append(defaultValue)
+        print(defaultValueList)
+
+        # INSERT INTO table_name  (field1, field2,...fieldN)  VALUES  (valueA1,valueA2,...valueAN),(valueB1,valueB2,...valueBN),(valueC1,valueC2,...valueCN)......;
+
+
+        pass
 
 
 class Rule(TestPlatform):
@@ -71,19 +128,20 @@ class Rule(TestPlatform):
         tableField = [item for item in self.tableFields] if tableField == '*' else tableField
         return self.query(tableName=self.tableName, tableField=tableField, keyWord=keyWord)
 
-    @logging("添加rule数据")
+    @logging("添加一个rule数据")
     def add_rule(self, data):
-        keysList = [key for key in data.keys()]
-        valueList = [value for value in data.values()]
-        print(type(keysList), keysList)
-        print(type(valueList), valueList)
-        pass
-        f"insert into students(name,age) values('王二小',15); "
+        return self.addOnce(self.tableName, data)
 
+    @logging("批量添加rule数据")
+    def adds_rule(self, dataList):
+        return self.addBatch(self.tableName, dataList)
 
 
 if __name__ == '__main__':
 
     rule = Rule()
-    print(rule.query_rule({'ruleName': '微众身份证影印件合规规则'}))
-    print(rule.add_rule({'details': '测试'}))
+    # print(rule.query_rule({'ruleName': '微众身份证影印件合规规则'}))
+    # print(rule.query_rule())
+    # print(rule.add_rule({'ruleName': '测试规则'}))
+    dataList = [{'ruleName': '微众身份证影印件合规规则'}, {'ruleName': '零钱提额用户的二类卡日剩余额度规则',}]
+    rule.adds_rule(dataList)
